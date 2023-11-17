ef48a30369d7ee582edf84eecb36ac1c183256e0
Moritz Rose <moritz_rose@yahoo.de>
2023-11-17 10:37:34 +0100

    fixed timestamp getter

--- src/main/java/com/brights/zwitscher/blogposts/BlogPostRepository.java
+++ src/main/java/com/brights/zwitscher/blogposts/BlogPostRepository.java
@@ -14,6 +14,6 @@ import java.util.Stack;
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost,Long> {
 
-    @Query(value = "SELECT * FROM BLOG_POST ORDER BY BLOG_POST.timestamp DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM BLOG_POST ORDER BY BLOG_POST_TIME DESC", nativeQuery = true)
     List<BlogPost> findAllOrderedByTimestampDesc();
 }
